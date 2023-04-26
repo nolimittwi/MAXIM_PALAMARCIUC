0d6422f0ee0cf2d5e3e9017a698606e960b4a897
gh3rtzz <99085132+gh3rtzz@users.noreply.github.com>
2023-04-26 21:37:31 +0300

    Delete src/main/java/org/example/homework_nr_9 directory (#10)

--- src/main/java/org/example/homework_nr_9/MeetingRoom.java
+++
@@ -1,7 +0,0 @@
-package org.example.homework_nr_9;
-
-import java.util.ArrayList;
-
-public class MeetingRoom {
-
-}
--- src/main/java/org/example/homework_nr_9/Teaching.java
+++
@@ -1,9 +0,0 @@
-package org.example.homework_nr_9;
-
-public interface Teaching {
-    default void teach(){
-        System.out.println("Employee is teaching");
-    }
-
-    void Teach();
-}
